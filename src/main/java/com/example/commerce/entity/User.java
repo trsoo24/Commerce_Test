458efe5264972319ce86d90700eeb63eb0783c33
trsoo24 <trsoo24@naver.com>
2024-04-25 17:58:10 +0900

    Feat : User Entity 생성

---
+++ src/main/java/com/example/commerce/entity/User.java
@@ -0,0 +1,38 @@
+package com.example.commerce.entity;
+
+import com.example.commerce.entity.dto.SignUpDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String userId;
+    private String password;
+    @Column(unique = true)
+    private String nickname;
+    private String name;
+    @Column(unique = true)
+    private String phoneNumber;
+    @Column(unique = true)
+    private String email;
+
+    public User toUser(SignUpDto dto) {
+        return User.builder()
+                .userId(dto.getUserId())
+                .password(dto.getPassword())
+                .nickname(dto.getNickname())
+                .name(dto.getName())
+                .phoneNumber(dto.getPhoneNumber())
+                .email(dto.getEmail())
+                .build();
+    }
+}
