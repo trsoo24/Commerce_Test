ed728fc541b2566f44667fb3a2ce9bdb18442da9
trsoo24 <trsoo24@naver.com>
2024-04-25 18:00:37 +0900

    Feat : User Service 생성

---
+++ src/main/java/com/example/commerce/service/UserService.java
@@ -0,0 +1,26 @@
+package com.example.commerce.service;
+
+import com.example.commerce.entity.User;
+import com.example.commerce.entity.dao.SignUpDao;
+import com.example.commerce.entity.dto.SignUpDto;
+import com.example.commerce.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final CheckUserReference checkUserReference;
+
+    public SignUpDao signUp (SignUpDto signUpDto) {
+        checkUserReference.existUserId(signUpDto.getUserId());
+        checkUserReference.existNickname(signUpDto.getNickname());
+        checkUserReference.existPhoneNumber(signUpDto.getPhoneNumber());
+        checkUserReference.checkEmail(signUpDto.getEmail());
+
+        userRepository.save(new User().toUser(signUpDto));
+
+        return new SignUpDao().toDao(signUpDto);
+    }
+}
