e04137d8f3fb2da0e3d741af2a16bf95f5e78267
trsoo24 <trsoo24@naver.com>
2024-04-25 19:51:00 +0900

    Refactor : Swagger UI schema 값 설명 추가

--- src/main/java/com/example/commerce/entity/dao/SignUpDao.java
+++ src/main/java/com/example/commerce/entity/dao/SignUpDao.java
@@ -2,20 +2,27 @@ package com.example.commerce.entity.dao;
 
 
 import com.example.commerce.entity.dto.SignUpDto;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Schema(description = "회원가입 응답 값")
 @Builder
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class SignUpDao {
+    @Schema(description = "아이디")
     private String userId;
+    @Schema(description = "닉네임")
     private String nickname;
+    @Schema(description = "이름")
     private String name;
+    @Schema(description = "전화번호")
     private String phoneNumber;
+    @Schema(description = "이메일")
     private String email;
 
     public SignUpDao toDao (SignUpDto dto) {
