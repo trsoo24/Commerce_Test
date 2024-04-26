50a3a5c7a9c857c0d5ee060aa9f6565ea76efdf4
trsoo24 <trsoo24@naver.com>
2024-04-26 14:26:51 +0900

    Refactor : 유효성 검사 Pattern Dto 클래스로 변경

--- src/main/java/com/example/commerce/service/CheckUserReference.java
+++ src/main/java/com/example/commerce/service/CheckUserReference.java
@@ -5,8 +5,6 @@ import com.example.commerce.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.regex.Pattern;
-
 import static com.example.commerce.exception.CustomCode.*;
 
 @Service
@@ -27,30 +25,14 @@ public class CheckUserReference {
     }
 
     void existPhoneNumber(String phoneNumber) {
-        checkPhoneNumberForm(phoneNumber);
         if (userRepository.existsByPhoneNumber(phoneNumber)) {
             throw new CustomException(DUPLICATED_PHONE_NUMBER);
         }
     }
 
     void checkEmail(String email) {
-        checkEmailForm(email);
         if (userRepository.existsByEmail(email)) {
             throw new CustomException(DUPLICATED_EMAIL);
-        }
-    }
-
-    void checkPhoneNumberForm(String phoneNumber) { // 전화번호 양식 확인 010-1234-1234
-        String pattern = "^\\d{3}-\\d{4}-\\d{4}$";
-        if (!Pattern.matches(pattern, phoneNumber)) {
-            throw new CustomException(NEED_MODIFY_PHONE_NUMBER_FORM);
-        }
-    }
-
-    void checkEmailForm(String email) { // 이메일 양식 확인 email@email.com
-        String pattern = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$";
-        if (!Pattern.matches(pattern, email)) {
-            throw new CustomException(NEED_MODIFY_EMAIL_FORM);
         }
     }
 }
