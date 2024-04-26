ca61359951c62124f50bb86cd0655c5ce52eda9d
trsoo24 <trsoo24@naver.com>
2024-04-26 14:19:40 +0900

    Feat : JPA findAll() 추가

--- src/main/java/com/example/commerce/repository/UserRepository.java
+++ src/main/java/com/example/commerce/repository/UserRepository.java
@@ -4,10 +4,13 @@ import com.example.commerce.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUserId(String userId);
     boolean existsByNickname(String nickname);
     boolean existsByPhoneNumber(String phoneNumber);
     boolean existsByEmail(String email);
+    List<User> findAll();
 }
