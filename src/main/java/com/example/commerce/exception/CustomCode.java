6da7df1ed19da8b61786b63493a3e85b055b7cf8
trsoo24 <trsoo24@naver.com>
2024-04-26 18:22:07 +0900

    Feat : USER_NOT_FOUND 예외 처리

--- src/main/java/com/example/commerce/exception/CustomCode.java
+++ src/main/java/com/example/commerce/exception/CustomCode.java
@@ -17,7 +17,10 @@ public enum CustomCode {
     DUPLICATED_PHONE_NUMBER(HttpStatus.BAD_REQUEST, "이미 가입된 휴대폰 번호입니다."),
     DUPLICATED_EMAIL(HttpStatus.BAD_REQUEST, "이미 가입된 이메일입니다."),
     NEED_MODIFY_PHONE_NUMBER_FORM(HttpStatus.BAD_REQUEST, "올바르지 않은 전화번호 양식입니다. 010-1234-1234"),
-    NEED_MODIFY_EMAIL_FORM(HttpStatus.BAD_REQUEST, "올바르지 않은 이메일 양식입니다. user@email.com")
+    NEED_MODIFY_EMAIL_FORM(HttpStatus.BAD_REQUEST, "올바르지 않은 이메일 양식입니다. user@email.com"),
+
+    // 유저 조회 실패
+    USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "찾을 수 없는 유저입니다.")
     ;
 
     private final HttpStatus status;
