209eecf97bb90835d0eef0196eff4925596997c3
trsoo24 <trsoo24@naver.com>
2024-04-26 18:23:17 +0900

    Feat : 유저 정보 수정 API 추가

--- src/main/java/com/example/commerce/service/UserService.java
+++ src/main/java/com/example/commerce/service/UserService.java
@@ -2,6 +2,7 @@ package com.example.commerce.service;
 
 import com.example.commerce.entity.User;
 import com.example.commerce.entity.dao.UserDao;
+import com.example.commerce.entity.dto.ModifyUserDto;
 import com.example.commerce.entity.dto.SignUpDto;
 import com.example.commerce.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +24,26 @@ public class UserService {
         checkUserReference.checkEmail(signUpDto.getEmail());
 
         User user = new User().toUser(signUpDto);
+        userRepository.save(user);
+
+        return new UserDao().toDao(user);
+    }
+
+    public UserDao modifyUserData (String userId, ModifyUserDto modifyUserDto) {
+        User user = checkUserReference.findByUserId(userId);
+
+        if (modifyUserDto.getPassword() != null) {
+            user.modifyPassword(modifyUserDto.getPassword());
+        }
+        if (modifyUserDto.getNickname() != null) {
+            user.modifyNickname(modifyUserDto.getNickname());
+        }
+        if (modifyUserDto.getEmail() != null) {
+            user.modifyEmail(modifyUserDto.getEmail());
+        }
+        if (modifyUserDto.getPhoneNumber() != null) {
+            user.modifyPhoneNumber(modifyUserDto.getPhoneNumber());
+        }
         userRepository.save(user);
 
         return new UserDao().toDao(user);
